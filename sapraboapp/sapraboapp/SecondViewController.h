af567a10ffc2592f2d2b2ed69f84819ffb8ad535
louenas <louenas.hamdi@bazo.ca>
2014-07-05 12:30:38 +0200

    nothing special

---
+++ sapraboapp/sapraboapp/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  sapraboapp
+//
+//  Created by Hamdi, Louenas on 2014-07-05.
+//  Copyright (c) 2014 SAP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
